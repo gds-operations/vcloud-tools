b0ef1525abada72abfdc97a5e420536f014c3daa
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-11-25 14:36:26 +0000

    set up for integration test running

---
+++ scripts/generate_fog_conf_file.sh
@@ -0,0 +1,6 @@
+cat <<EOF >fog_integration_test.config
+default:
+    vcloud_director_username: '$API_USERNAME'
+    vcloud_director_password: '$API_PASSWORD'
+    vcloud_director_host: 'api.vcd.portal.skyscapecloud.com'
+EOF
