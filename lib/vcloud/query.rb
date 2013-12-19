be0e9330e04fec7631b8a2d71a4e72b3f9edb431
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-12-19 14:26:37 +0000

    replacing abort with raise exception

--- lib/vcloud/query.rb
+++ lib/vcloud/query.rb
@@ -62,7 +62,7 @@ module Vcloud
         body = @fsi.get_execute_query(type=@type, @options.merge({:page=>page}))
         pp body if @options[:debug]
       rescue ::Fog::Compute::VcloudDirector::BadRequest, ::Fog::Compute::VcloudDirector::Forbidden => e
-        Kernel.abort("#{File.basename($0)}: #{e.message}")
+        raise "Access denied: #{e.message}"
       end
 
       records = body.keys.detect {|key| key.to_s =~ /Record|Reference$/}
