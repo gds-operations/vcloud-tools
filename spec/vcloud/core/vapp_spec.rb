8eed0fc70ece8eebf025080d7f57b020f92f65a1
Mike Pountney <Mike.Pountney@gmail.com>
2013-12-21 17:20:29 +0000

    Restructure Vapp class to be immutable
    * Improve vapp_spec readability and highlight public Class and Instance interface
    * Remove now-unneccessary reload() method, since we no longer keep state to reload
    * Move methods that find or create Vapps into the class, returning vapp instances
    
    The main purpose of this commit is to remove any mutable state from the class, specifically the handling of @vcloud_attributes.
    
    Previously, we were in effect caching the output of get_vapp into @vcloud_attributes, but not really handling the fact that the state of the Vapp could change underneath us. Notionally the reload() method allowed us to update our inner state, but not consistently.
    
    Also have removed @fog_interface from the state, since this hold onto Fog objects and (tbc) does not handle the case when a session token expires (Fog may do this automatically though)
    
    In the process, have also been pushed the methods that find and create Vapps into the class, effectively as factory methods. They should definitely not be instance methods, but arguably can be later farmed out into a separate vapp factory class.

--- spec/vcloud/core/vapp_spec.rb
+++ spec/vcloud/core/vapp_spec.rb
@@ -4,15 +4,84 @@ module Vcloud
   module Core
     describe Vapp do
       before(:each) do
+        @vapp_id = 'vapp-12345678-1234-1234-1234-000000111111'
         @mock_fog_interface = StubFogInterface.new
         Vcloud::Fog::ServiceInterface.stub(:new).and_return(@mock_fog_interface)
       end
 
+      context "Class public interface" do
+        it { Vapp.should respond_to(:instantiate) }
+        it { Vapp.should respond_to(:get_by_name) }
+        it { Vapp.should respond_to(:get_metadata) }
+      end
+
+      context "Instance public interface" do
+        subject { Vapp.new(@vapp_id) }
+        it { should respond_to(:id) }
+        it { should respond_to(:vcloud_attributes) }
+        it { should respond_to(:name) }
+        it { should respond_to(:href) }
+        it { should respond_to(:vdc_id) }
+        it { should respond_to(:fog_vms) }
+        it { should respond_to(:networks) }
+        it { should respond_to(:power_on) }
+      end
+
+      context "#initialize" do
+
+        it "should fail if constructed with no args" do
+          expect{ Vapp.new() }.to raise_exception(ArgumentError)
+        end
+
+        it "should be constructable from just an id reference" do
+          obj = Vapp.new(@vapp_id)
+          expect(obj.class).to be(Vcloud::Core::Vapp)
+        end
+
+        it "should raise error if id is not in correct format" do
+          bogus_id = '12314124-ede5-4d07-bad5-000000111111'
+          expect{ Vapp.new(bogus_id) }.to raise_error("vapp id : #{bogus_id} is not in correct format" )
+        end
+
+      end
+
+      context "#get_by_name" do
+
+        it "should fail if called with no args" do
+          expect { Vapp.get_by_name() }.to raise_exception(ArgumentError)
+        end
+
+        it "should return a Vapp object if name exists" do
+          q_results = [
+            { :name => 'vapp-test-1', :href => @vapp_id }
+          ]
+          Vcloud::Query.any_instance.stub(:get_all_results).and_return(q_results)
+          obj = Vapp.get_by_name('vapp-test-1')
+          expect(obj.class).to be(Vcloud::Core::Vapp)
+        end
+
+        it "should raise an error if no vApp with that name exists" do
+          q_results = [ ]
+          Vcloud::Query.any_instance.stub(:get_all_results).and_return(q_results)
+          expect{ Vapp.get_by_name('vapp-test-1') }.to raise_exception(RuntimeError)
+        end
+
+        it "should raise an error if multiple vApps with that name exists (NB: prescribes unique vApp names!)" do
+          q_results = [
+            { :name => 'vapp-test-1', :href => @vapp_id },
+            { :name => 'vapp-test-1', :href => '/bogus' },
+          ]
+          Vcloud::Query.any_instance.stub(:get_all_results).and_return(q_results)
+          expect{ Vapp.get_by_name('vapp-test-1') }.to raise_exception(RuntimeError)
+        end
+
+      end
+
       context "attributes" do
         before(:each) {
-          attributes = {
+          @stub_attrs = {
               :name => 'Webserver vapp-1',
-              :href => 'https://api.vcd.portal.skyscapecloud.com/api/vApp/vapp-7647f3ab-ede5-4d07-bad5-4e2235800ef3',
+              :href => "https://api.vcd.portal.skyscapecloud.com/api/vApp/#{@vapp_id}",
               :Link => [{
                             :rel => 'up',
                             :type => 'application/vnd.vmware.vcloud.vdc+xml',
@@ -20,29 +89,25 @@ module Vcloud
                         }],
               :Children => {:Vm => [{:href => '/vm-123aea1e-a5e9-4dd1-a028-40db8c98d237'}]}
           }
-          @vapp = Vapp.new(attributes)
+          StubFogInterface.any_instance.stub(:get_vapp).and_return(@stub_attrs)
+          @vapp = Vapp.new(@vapp_id)
         }
-        it { @vapp.should respond_to(:vcloud_attributes) }
         it { @vapp.name.should == 'Webserver vapp-1' }
 
         context "id" do
           it "should extract id correctly" do
-            @vapp.id.should == 'vapp-7647f3ab-ede5-4d07-bad5-4e2235800ef3'
-          end
-
-          it "should raise error if id is not in correct format" do
-            @vapp = Vapp.new({:href => 'https://api.vcd.portal.skyscapecloud.com/api/vApp/7647f3ab-ede5-4d07-bad5-4e2235800ef3' })
-            lambda { @vapp.id }.should raise_error("vapp id : 7647f3ab-ede5-4d07-bad5-4e2235800ef3 is not in correct format" )
+            @vapp.id.should == @vapp_id
           end
         end
+
         context "vapp should have parent vdc" do
           it "should load parent vdc id from fog attributes" do
             @vapp.vdc_id.should == '074aea1e-a5e9-4dd1-a028-40db8c98d237'
           end
 
           it "should raise error if vapp without parent vdc found" do
-            vapp_without_vdc = Vcloud::Core::Vapp.new({:Link => []})
-            lambda { vapp_without_vdc.vdc_id }.should raise_error('a vapp without parent vdc found')
+            @stub_attrs[:Link] = []
+            lambda { @vapp.vdc_id }.should raise_error('a vapp without parent vdc found')
           end
         end
 
@@ -67,7 +132,7 @@ module Vcloud
           end
 
           it "should power on a vapp that is not powered on" do
-            vapp = Vapp.new(@fog_vapp_body)
+            vapp = Vapp.new(@vapp_id)
             @mock_fog_interface.should_receive(:get_vapp).twice().and_return({:status => 8})
             @mock_fog_interface.should_receive(:power_on_vapp).with(vapp.id)
             state = vapp.power_on
@@ -75,7 +140,7 @@ module Vcloud
           end
 
           it "should not power on a vapp that is already powered on, but should return true" do
-            vapp = Vapp.new(@fog_vapp_body)
+            vapp = Vapp.new(@vapp_id)
             @mock_fog_interface.should_receive(:get_vapp).and_return({:status => 4})
             @mock_fog_interface.should_not_receive(:power_on_vapp)
             state = vapp.power_on
@@ -91,18 +156,15 @@ module Vcloud
           StubFogInterface.any_instance.stub(:get_vapp_by_name_and_vdc_name)
             .with('vapp_name', 'vdc_name').and_return(nil)
           Vapp.get_by_name_and_vdc_name('vapp_name', 'vdc_name').should == nil
-
         end
 
         it "should return vapp instance if found" do
-
-          vcloud_attr_vapp = {:id => 1}
+          vcloud_attr_vapp = { :href => "/#{@vapp_id}" }
           StubFogInterface.any_instance.stub(:get_vapp_by_name_and_vdc_name)
-          .with('vapp_name', 'vdc_name').and_return(vcloud_attr_vapp)
-
+            .with('vapp_name', 'vdc_name').and_return(vcloud_attr_vapp)
           Vapp.get_by_name_and_vdc_name('vapp_name', 'vdc_name').class.should == Core::Vapp
-
         end
+
       end
 
     end
