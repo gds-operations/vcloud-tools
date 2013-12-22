f94b27fe193ab29d26e24c28077b2f20ea5c3bfe
Mike Pountney <Mike.Pountney@gmail.com>
2013-12-22 20:13:20 +0000

    fix up VappOrchestrator unit test to always run against a stub fog interface

--- spec/vcloud/vapp_orchestrator_spec.rb
+++ spec/vcloud/vapp_orchestrator_spec.rb
@@ -4,9 +4,14 @@ module Vcloud
 
   describe VappOrchestrator do
 
+    before(:each) do
+      @mock_fog_interface = StubFogInterface.new
+      Vcloud::Fog::ServiceInterface.stub(:new).and_return(@mock_fog_interface)
+    end
+
     context "provision a vapp" do
 
-      before(:all) do
+      before(:each) do
         @config = {
             :name => 'test-vapp-1',
             :vdc_name => 'test-vdc-1',
@@ -22,6 +27,7 @@ module Vcloud
         existing_vapp = double(:vapp, :name => 'existing-vapp-1')
 
         Core::Vapp.should_receive(:get_by_name_and_vdc_name).with('test-vapp-1', 'test-vdc-1').and_return(existing_vapp)
+        Vcloud.logger.should_receive(:info).with('Found existing vApp test-vapp-1 in vDC \'test-vdc-1\'. Skipping.')
         actual_vapp = VappOrchestrator.provision @config
         actual_vapp.should_not be_nil
         actual_vapp.should == existing_vapp
