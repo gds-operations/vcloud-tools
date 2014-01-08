6cb6b7128cfdbd003c6bb67f4ce1e0636739b934
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-08 12:15:43 +0000

    fixed indentation in vapp_orchestrator

--- lib/vcloud/vapp_orchestrator.rb
+++ lib/vcloud/vapp_orchestrator.rb
@@ -4,16 +4,16 @@ module Vcloud
     def self.provision(vapp_config)
       name, vdc_name = vapp_config[:name], vapp_config[:vdc_name]
 
-        if vapp = Vcloud::Core::Vapp.get_by_name_and_vdc_name(name, vdc_name)
-          Vcloud.logger.info("Found existing vApp #{name} in vDC '#{vdc_name}'. Skipping.")
-        else
-          template = Vcloud::Core::VappTemplate.get(vapp_config[:catalog], vapp_config[:catalog_item])
-          template_id = template.id
+      if vapp = Vcloud::Core::Vapp.get_by_name_and_vdc_name(name, vdc_name)
+        Vcloud.logger.info("Found existing vApp #{name} in vDC '#{vdc_name}'. Skipping.")
+      else
+        template = Vcloud::Core::VappTemplate.get(vapp_config[:catalog], vapp_config[:catalog_item])
+        template_id = template.id
 
-          network_names = extract_vm_networks(vapp_config)
-          vapp = Vcloud::Core::Vapp.instantiate(name, network_names, template_id, vdc_name)
-          Vcloud::VmOrchestrator.new(vapp.fog_vms.first, vapp).customize(vapp_config[:vm]) if vapp_config[:vm]
-        end
+        network_names = extract_vm_networks(vapp_config)
+        vapp = Vcloud::Core::Vapp.instantiate(name, network_names, template_id, vdc_name)
+        Vcloud::VmOrchestrator.new(vapp.fog_vms.first, vapp).customize(vapp_config[:vm]) if vapp_config[:vm]
+      end
       vapp
     end
 
