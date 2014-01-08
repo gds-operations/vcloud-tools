6d8c99cf125db05cb26b08f2479caebd21e8198e
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-08 12:13:02 +0000

    got rid of unnecessary instance variable

--- lib/vcloud/launch.rb
+++ lib/vcloud/launch.rb
@@ -7,20 +7,19 @@ module Vcloud
       @config_loader = Vcloud::ConfigLoader.new
     end
 
-    def run(config_file = nil, options = {})
-      @cli_options = options
+    def run(config_file = nil, cli_options = {})
+      puts "cli_options:" if cli_options[:debug]
+      pp cli_options if cli_options[:debug]
 
-      puts "cli_options:" if @cli_options[:debug]
-      pp @cli_options if @cli_options[:debug]
       config = @config_loader.load_config(config_file)
       config[:vapps].each do |vapp_config|
         Vcloud.logger.info("Configuring vApp #{vapp_config[:name]}.")
         begin
           vapp = ::Vcloud::VappOrchestrator.provision(vapp_config)
-          vapp.power_on unless @cli_options[:no_power_on]
+          vapp.power_on unless cli_options[:no_power_on]
         rescue RuntimeError => e
           Vcloud.logger.error("Could not provision vApp: #{e.message}")
-          break unless options[:continue_on_error]
+          break unless cli_options[:continue_on_error]
         end
       end
     end
