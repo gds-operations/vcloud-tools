3931d3ac194902532264dc6f9d87f432b7f86e9e
Mike Pountney <Mike.Pountney@gmail.com>
2014-01-14 16:27:52 +0000

    [#59505526] String validation

--- lib/vcloud/config_validator.rb
+++ lib/vcloud/config_validator.rb
@@ -1,18 +1,36 @@
 module Vcloud
   class ConfigValidator
 
-    def initialize(data, schema)
+    attr_reader :data, :schema, :errors
+
+    def initialize(key, data, schema)
       @errors = {}
       @data   = data
       @schema = schema
+      @key    = key
+      validate
     end
 
     def valid?
       @errors.empty?
     end
 
-    def self.validate(data, schema)
-      new(data, schema)
+    def validate
+      raise "Invalid schema" unless @schema.key?(:type)
+      type = @schema[:type].downcase
+      self.send("validate_#{type}".to_sym)
+    end
+
+    def self.validate(key, data, schema)
+      new(key, data, schema)
+    end
+
+    private
+
+    def validate_string
+      unless @data.is_a? String
+        @errors[@key] = "#{@data} is not a string"
+      end
     end
 
   end
--- spec/vcloud/config_validator_spec.rb
+++ spec/vcloud/config_validator_spec.rb
@@ -3,11 +3,29 @@ require 'spec_helper'
 module Vcloud
   describe ConfigValidator do
 
-    it "should validate a basic string" do
-      data = "hello world"
-      schema = { type: String }
-      v = ConfigValidator.validate(data, schema)
-      expect(v.valid?).to be_true
+    context "string validations" do
+
+      it "should validate a basic string" do
+        data = "hello world"
+        schema = { type: "String" }
+        v = ConfigValidator.validate(:base, data, schema)
+        expect(v.valid?).to be_true
+      end
+
+      it "should not validate a number as a basic string" do
+        data = 42
+        schema = { type: "String" }
+        v = ConfigValidator.validate(:base, data, schema)
+        expect(v.valid?).to be_false
+      end
+
+      it "should log error with number as a basic string" do
+        data = 42
+        schema = { type: "String" }
+        v = ConfigValidator.validate(:base, data, schema)
+        expect(v.errors).to eq({ :base => '42 is not a string' } )
+      end
+
     end
 
   end
