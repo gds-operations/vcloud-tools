a332e8101e12c0351fa0a452053c51021e410f0c
Mike Pountney <Mike.Pountney@gmail.com>
2014-01-15 13:38:17 +0000

    [#59505526] enable schema checking in Vcloud::Launch
    If a schema is supplied to config_loader, then we will check against
    it, log errors, and raise an exception if validation fails.
    
    Added tests in config_loader_spec.rb to check validation, and also to
    check validation failure.
    
    The tests for config_loader are very specific to Vcloud::Launch though,
    it might be worth splitting these out into their respective modules later.
    This could happen when we switch Vcloud::NetLaunch to use validation too.

--- lib/vcloud/config_loader.rb
+++ lib/vcloud/config_loader.rb
@@ -3,12 +3,23 @@ require 'vcloud'
 module Vcloud
   class ConfigLoader
 
-    def load_config(config_file)
-      config = YAML::load(File.open(config_file))
+    def load_config(config_file, schema = nil)
+      input_config = YAML::load(File.open(config_file))
 
       # There is no way in YAML or Ruby to symbolize keys in a hash
-      json_string = JSON.generate(config)
-      JSON.parse(json_string, :symbolize_names => true)
+      json_string = JSON.generate(input_config)
+      config = JSON.parse(json_string, :symbolize_names => true)
+
+      if schema
+        validation = ConfigValidator.validate(:base, config, schema)
+        unless validation.valid?
+          validation.errors.each do |error|
+            Vcloud.logger.fatal(error)
+          end
+          raise("Supplied configuration does not match supplied schema")
+        end
+      end
+      config
     end
 
   end 
--- spec/vcloud/config_loader_spec.rb
+++ spec/vcloud/config_loader_spec.rb
@@ -13,7 +13,6 @@ describe Vcloud::ConfigLoader do
     valid_config.should eq(actual_config)
   end
 
-
   it "should create a valid hash when input is YAML" do
     input_file = 'spec/vcloud/data/working.yaml'
     loader = Vcloud::ConfigLoader.new
@@ -26,6 +25,32 @@ describe Vcloud::ConfigLoader do
     loader = Vcloud::ConfigLoader.new
     actual_config = loader.load_config(input_file)
     valid_config['vapps'].should eq(actual_config['vapps'])
+  end
+
+  it "should validate correctly against a schema" do
+    input_file = 'spec/vcloud/data/working_with_defaults.yaml'
+    loader = Vcloud::ConfigLoader.new
+    schema = Vcloud::Launch.new.config_schema
+    actual_config = loader.load_config(input_file, schema)
+    valid_config['vapps'].should eq(actual_config['vapps'])
+  end
+
+  it "should raise an error if checked against an invalid schema" do
+    input_file = 'spec/vcloud/data/working_with_defaults.yaml'
+    loader = Vcloud::ConfigLoader.new
+    Vcloud.logger.should_receive(:fatal).with("vapps: is not a hash")
+    expect { loader.load_config(input_file, invalid_schema) }.
+      to raise_error('Supplied configuration does not match supplied schema')
+  end
+
+  def invalid_schema
+    {
+      type: Hash,
+      permit_unknown_parameters: true,
+      internals: {
+        vapps: { type: Hash },
+      }
+    }
   end
 
   def valid_config
