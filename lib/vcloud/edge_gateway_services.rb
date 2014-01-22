918ce2baa367cc8815d96b58acde418fc200d248
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-22 17:10:40 +0000

    @snehaso/@mikepea Add EdgeGateway update functionality
    ... including passing integration tests that ensure they
    run on a clean edgeGateway (resetting firewall rules before+after)

---
+++ lib/vcloud/edge_gateway_services.rb
@@ -0,0 +1,19 @@
+require 'vcloud'
+
+module Vcloud
+  class EdgeGatewayServices
+
+    def initialize
+      @config_loader = Vcloud::ConfigLoader.new
+    end
+
+    def update(config_file = nil, options = {})
+      config = @config_loader.load_config(config_file, Vcloud::Schema::EDGE_GATEWAY_SERVICES)
+      firewall_service_config = EdgeGateway::FirewallConfigurationGenerator.new.firewall_config(config[:firewall_service])
+      edge_gateway = Core::EdgeGateway.get_by_name config[:gateway]
+
+      edge_gateway.update_configuration({:FirewallService => firewall_service_config})
+    end
+
+  end
+end
---
+++ spec/integration/edge_gateway/edge_gateway_services_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+module Vcloud
+
+
+  describe EdgeGatewayServices do
+    it "raise exception if input yaml does not match with schema" do
+      config_erb = File.expand_path('data/incorrect_firewall_config.yaml.erb', File.dirname(__FILE__))
+      input_config_file = generate_input_yaml_config({:edge_gateway_name => ENV['VCLOUD_EDGE_GATEWAY']}, config_erb)
+      expect { EdgeGatewayServices.new.update(input_config_file) }.to raise_error('Supplied configuration does not match supplied schema')
+    end
+
+    context "configure firewall service" do
+      before(:all) do
+        reset_firewall
+      end
+
+      it "should configure firewall service" do
+        config_erb = File.expand_path('data/firewall_config.yaml.erb', File.dirname(__FILE__))
+        input_config_file = generate_input_yaml_config({:edge_gateway_name => ENV['VCLOUD_EDGE_GATEWAY']}, config_erb)
+        EdgeGatewayServices.new.update(input_config_file)
+
+        edge_gateway = Vcloud::Core::EdgeGateway.get_by_name(ENV['VCLOUD_EDGE_GATEWAY'])
+
+        firewall_service = edge_gateway.vcloud_attributes[:Configuration][:EdgeGatewayServiceConfiguration][:FirewallService]
+        expect(firewall_service.key?(:FirewallRule)).to be_true
+        expect(firewall_service[:FirewallRule]).to eq([{:Id => "1",
+                                                        :IsEnabled => "true",
+                                                        :MatchOnTranslate => "false",
+                                                        :Description => "A rule",
+                                                        :Policy => "allow",
+                                                        :Protocols => {:Tcp => "true"},
+                                                        :Port => "-1",
+                                                        :DestinationPortRange => "Any",
+                                                        :DestinationIp => "10.10.1.2",
+                                                        :SourcePort => "-1",
+                                                        :SourcePortRange => "Any",
+                                                        :SourceIp => "192.0.2.2",
+                                                        :EnableLogging => "false"}])
+      end
+
+      after(:all) do
+        reset_firewall
+      end
+    end
+
+    def reset_firewall
+      edge_gateway = Core::EdgeGateway.get_by_name ENV['VCLOUD_EDGE_GATEWAY']
+      edge_gateway.update_configuration({ FirewallService: { IsEnabled: false, FirewallRule: []} })
+    end
+
+    def generate_input_yaml_config test_namespace, input_erb_config
+      input_erb_config = input_erb_config
+      e = ERB.new(File.open(input_erb_config).read)
+      output_yaml_config = File.join(File.dirname(input_erb_config), "output_#{Time.now.strftime('%s')}.yaml")
+      File.open(output_yaml_config, 'w') { |f|
+        f.write e.result(OpenStruct.new(test_namespace).instance_eval { binding })
+      }
+      output_yaml_config
+    end
+  end
+end
