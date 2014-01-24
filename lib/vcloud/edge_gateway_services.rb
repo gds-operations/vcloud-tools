7f881f1eb08f61027839f49fc5168c5d6df90b4c
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-24 11:22:18 +0000

    renamed method
    renamed FirewallService.new.firewall_config(i) to
    FirewallService.new.generate_fog_config(i)

--- lib/vcloud/edge_gateway_services.rb
+++ lib/vcloud/edge_gateway_services.rb
@@ -26,7 +26,7 @@ module Vcloud
     private
     def translate_yaml_input(config_file)
       config = @config_loader.load_config(config_file, Vcloud::Schema::EDGE_GATEWAY_SERVICES)
-      firewall_service_config = EdgeGateway::ConfigurationGenerator::FirewallService.new.firewall_config(config[:firewall_service])
+      firewall_service_config = EdgeGateway::ConfigurationGenerator::FirewallService.new.generate_fog_config(config[:firewall_service])
       {:gateway => config[:gateway], :FirewallService => firewall_service_config}
     end
 
