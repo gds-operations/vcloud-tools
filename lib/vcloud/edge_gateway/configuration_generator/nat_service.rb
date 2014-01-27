826d9b0c6cb7941d4459213378b2d4a5882bdea2
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-27 14:47:41 +0000

    implemented nat_service configuration generator.

---
+++ lib/vcloud/edge_gateway/configuration_generator/nat_service.rb
@@ -0,0 +1,45 @@
+module Vcloud
+  module EdgeGateway
+    module ConfigurationGenerator
+
+      class NatService
+        def generate_fog_config input_config
+          if input_config
+            nat_service = {}
+            nat_service[:IsEnabled] = input_config.key?(:enabled) ? input_config[:enabled].to_s : 'true'
+            nat_service[:NatRule] = populate_nat_rules(input_config[:nat_rules])
+            nat_service
+          end
+          nat_service
+        end
+
+        def populate_nat_rules(rules)
+            i = 65537
+            rules.collect do |rule|
+              new_rule = {}
+              new_rule[:Id] = rule.key?(:id) ? rule[:id] : i.to_s
+              new_rule[:IsEnabled] = rule.key?(:enabled) ? rule[:enabled].to_s : 'true'
+              new_rule[:RuleType] = rule[:rule_type]
+              new_rule[:Description] = rule.key?(:description) ? rule[:description] : ""
+              gateway_nat_rule = populate_gateway_nat_rule(rule)
+              new_rule[:GatewayNatRule] = gateway_nat_rule
+              i += 1
+              new_rule
+          end
+        end
+
+        def populate_gateway_nat_rule(rule)
+          network_attrs = Vcloud::Core::OrgVdcNetwork.get_by_name(rule[:network]).vcloud_attributes
+          gateway_nat_rule = {:Interface => {:name => network_attrs[:name], :href => network_attrs[:href] }}
+          gateway_nat_rule[:OriginalIp] = rule[:original_ip]
+          gateway_nat_rule[:TranslatedIp] = rule[:translated_ip]
+          gateway_nat_rule[:OriginalPort] = rule[:original_port] if rule.key?(:original_port)
+          gateway_nat_rule[:TranslatedPort] = rule[:translated_port] if rule.key?(:translated_port)
+          gateway_nat_rule[:Protocol] = rule.key?(:Protocol) ? rule[:Protocol] : "tcp"
+          gateway_nat_rule
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/vcloud/edge_gateway/configuration_generator/nat_service_spec.rb
@@ -0,0 +1,152 @@
+require 'spec_helper'
+
+module Vcloud
+  module EdgeGateway
+    module ConfigurationGenerator
+      describe NatService do
+        context "nat service config generation" do
+
+          test_cases = [
+            {
+              title: 'enabled nat service with single disabled rule',
+              input: {
+                enabled: 'true',
+                nat_rules: [
+                  {
+                    enabled: 'false',
+                    id: '999',
+                    rule_type: 'DNAT',
+                    description: "a dnat rule",
+                    network: "ane012345",
+                    original_ip: "192.0.2.2",
+                    original_port: '22',
+                    translated_port: '22',
+                    translated_ip: "10.10.20.20",
+                    protocol: 'tcp',
+                  }
+                ]
+              },
+              output: {
+                :IsEnabled => 'true',
+                :NatRule => [
+                  {
+                    :Description => 'a dnat rule',
+                    :RuleType => 'DNAT',
+                    :IsEnabled => 'false',
+                    :Id => '999',
+                    :GatewayNatRule => {
+                      :Interface =>
+                        {
+                          :name => 'ane012345',
+                          :href => 'https://vmware.api.net/api/admin/network/2ad93597-7b54-43dd-9eb1-631dd337e5a7'
+                        },
+                      :Protocol => 'tcp',
+                      :OriginalIp => "192.0.2.2",
+                      :OriginalPort => '22',
+                      :TranslatedIp => "10.10.20.20",
+                      :TranslatedPort => '22'
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              title: 'auto generate rule id if not provided',
+              input: {
+                enabled: 'true',
+                nat_rules: [
+                  {
+                    enabled: 'false',
+                    rule_type: 'DNAT',
+                    description: "a dnat rule",
+                    network: "ane012345",
+                    original_ip: "192.0.2.2",
+                    original_port: '22',
+                    translated_port: '22',
+                    translated_ip: "10.10.20.20",
+                    protocol: 'tcp',
+                  }
+                ]
+              },
+              output: {
+                :IsEnabled => 'true',
+                :NatRule => [
+                  {
+                    :Description => 'a dnat rule',
+                    :RuleType => 'DNAT',
+                    :IsEnabled => 'false',
+                    :Id => '65537',
+                    :GatewayNatRule => {
+                      :Interface =>
+                        {
+                          :name => 'ane012345',
+                          :href => 'https://vmware.api.net/api/admin/network/2ad93597-7b54-43dd-9eb1-631dd337e5a7'
+                        },
+                      :Protocol => 'tcp',
+                      :OriginalIp => "192.0.2.2",
+                      :OriginalPort => '22',
+                      :TranslatedIp => "10.10.20.20",
+                      :TranslatedPort => '22'
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              title: 'should use default values for optional fields if they are missing',
+              input: {
+                nat_rules: [
+                  {
+                    rule_type: 'DNAT',
+                    network: "ane012345",
+                    original_ip: "192.0.2.2",
+                    original_port: '22',
+                    translated_port: '22',
+                    translated_ip: "10.10.20.20",
+                  }
+                ]
+              },
+              output: {
+                :IsEnabled => 'true',
+                :NatRule => [
+                  {
+                    :Description => '',
+                    :RuleType => 'DNAT',
+                    :IsEnabled => 'true',
+                    :Id => '65537',
+                    :GatewayNatRule => {
+                      :Interface =>
+                        {
+                          :name => 'ane012345',
+                          :href => 'https://vmware.api.net/api/admin/network/2ad93597-7b54-43dd-9eb1-631dd337e5a7'
+                        },
+                      :Protocol => 'tcp',
+                      :OriginalIp => "192.0.2.2",
+                      :OriginalPort => '22',
+                      :TranslatedIp => "10.10.20.20",
+                      :TranslatedPort => '22'
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+
+          test_cases.each do |test_case|
+            it "#{test_case[:title]}" do
+              expect(Vcloud::Core::OrgVdcNetwork).to receive(:get_by_name).with('ane012345')
+              .and_return(double(:network, :vcloud_attributes => {
+                :name => 'ane012345',
+                :href => 'https://vmware.api.net/api/admin/network/2ad93597-7b54-43dd-9eb1-631dd337e5a7'
+              }))
+              generated_config = NatService.new.generate_fog_config test_case[:input]
+              expect(generated_config).to eq(test_case[:output])
+            end
+
+          end
+
+        end
+      end
+    end
+  end
+end
