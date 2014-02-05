60dbca704e949ca934718683f852a51391b6dcee
Anna Shipman <anna@annashipman.co.uk>
2014-02-05 10:55:22 +0000

    Rename config to local_config for clarity

--- lib/vcloud/edge_gateway_services.rb
+++ lib/vcloud/edge_gateway_services.rb
@@ -16,12 +16,12 @@ module Vcloud
     end
 
     def update(config_file = nil, options = {})
-      config = translate_yaml_input(config_file)
-      edge_gateway = Core::EdgeGateway.get_by_name config[:gateway]
+      local_config = translate_yaml_input(config_file)
+      edge_gateway = Core::EdgeGateway.get_by_name local_config[:gateway]
       remote_config = edge_gateway.vcloud_attributes[:Configuration][:EdgeGatewayServiceConfiguration]
       diff_output = {}
       EdgeGatewayServices.edge_gateway_services.each do |service|
-        local = config[service]
+        local = local_config[service]
         remote = remote_config[service]
         differ = EdgeGateway::ConfigurationDiffer.new(local, remote)
         diff_output[service] = differ.diff
@@ -31,15 +31,15 @@ module Vcloud
       EdgeGatewayServices.edge_gateway_services.each do |service|
         # Skip services whose configuration has not changed, or that
         # are not specified in our source configuration.
-        if diff_output[service].empty? or not config.key?(service)
+        if diff_output[service].empty? or not local_config.key?(service)
           skipped_service_count += 1
-          config.delete(service)
+          local_config.delete(service)
         end
       end
       if skipped_service_count == EdgeGatewayServices.edge_gateway_services.size
         Vcloud.logger.info("EdgeGatewayServices.update: Configuration is already up to date. Skipping.")
       else
-        edge_gateway.update_configuration config
+        edge_gateway.update_configuration local_config
       end
     end
 
