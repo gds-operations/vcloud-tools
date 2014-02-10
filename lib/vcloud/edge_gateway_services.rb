21403847eb6e3aedfdb1e09e88a845c88c3ba1df
Anna Shipman <anna@annashipman.co.uk>
2014-02-10 20:45:41 +0000

    No need to set the gateway
    Not sure why this was set - possibly left over from an early iteration? But it's not used now.

--- lib/vcloud/edge_gateway_services.rb
+++ lib/vcloud/edge_gateway_services.rb
@@ -10,9 +10,10 @@ module Vcloud
 
     def update(config_file = nil)
       config = @config_loader.load_config(config_file, Vcloud::Schema::EDGE_GATEWAY_SERVICES)
-      local_config = { gateway: config[:gateway] }
+      local_config = { }
 
-      edge_gateway = Core::EdgeGateway.get_by_name local_config[:gateway]
+      edge_gateway = Core::EdgeGateway.get_by_name config[:gateway]
+
       remote_config = edge_gateway.vcloud_attributes[:Configuration][:EdgeGatewayServiceConfiguration]
 
       firewall_service_config = EdgeGateway::ConfigurationGenerator::FirewallService.new.generate_fog_config(config[:firewall_service])
