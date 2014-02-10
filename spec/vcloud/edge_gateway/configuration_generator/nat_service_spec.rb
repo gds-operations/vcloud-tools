fd7e0a17bfb08366153d2721f585605ddb8d156a
Mike Pountney <Mike.Pountney@gmail.com>
2014-02-10 12:03:43 +0000

    add basic DNAT/SNAT rule default tests
    * DRY out tests accordingly

--- spec/vcloud/edge_gateway/configuration_generator/nat_service_spec.rb
+++ spec/vcloud/edge_gateway/configuration_generator/nat_service_spec.rb
@@ -4,6 +4,63 @@ module Vcloud
   module EdgeGateway
     module ConfigurationGenerator
       describe NatService do
+
+        before(:each) do
+          @edge_id = '1111111-7b54-43dd-9eb1-631dd337e5a7'
+          edge_gateway = double(
+            :edge_gateway,
+            :vcloud_gateway_interface_by_id => {
+              Network: {
+                name: 'ane012345',
+                href: 'https://vmware.api.net/api/admin/network/2ad93597-7b54-43dd-9eb1-631dd337e5a7'
+              }
+            }
+          )
+          expect(Vcloud::Core::EdgeGateway).to receive(:get_by_name).with(@edge_id)
+                                               .and_return(edge_gateway)
+        end
+
+        context "SNAT rule defaults" do
+
+          before(:each) do
+            input = { nat_rules: [{
+              rule_type: 'SNAT',
+              network: "ane012345",
+              original_ip: "192.0.2.2",
+              translated_ip: "10.10.20.20",
+            }]} # minimum NAT configuration with a rule
+            output = NatService.new(@edge_id, input).generate_fog_config
+            @rule = output[:NatRule].first
+          end
+
+          it 'should default to the rule being enabled' do
+            expect(@rule[:IsEnabled]).to eq('true')
+          end
+
+        end
+
+        context "DNAT rule defaults" do
+
+          before(:each) do
+            input = { nat_rules: [{
+              rule_type: 'DNAT',
+              network: "ane012345",
+              original_ip: "192.0.2.2",
+              original_port: '22',
+              translated_port: '22',
+              translated_ip: "10.10.20.20",
+              protocol: 'tcp',
+            }]} # minimum NAT configuration with a rule
+            output = NatService.new(@edge_id, input).generate_fog_config
+            @rule = output[:NatRule].first
+          end
+
+          it 'should default to rule being enabled' do
+            expect(@rule[:IsEnabled]).to eq('true')
+          end
+
+        end
+
         context "nat service config generation" do
 
           test_cases = [
@@ -124,27 +181,15 @@ module Vcloud
                   }
                 ]
               }
+
             }
           ]
 
           test_cases.each do |test_case|
             it "#{test_case[:title]}" do
-              edge_gateway = double(:edge_gateway,
-                                    :vcloud_gateway_interface_by_id =>
-                                      {
-                                        Network:
-                                          {
-                                            :name => 'ane012345',
-                                            :href => 'https://vmware.api.net/api/admin/network/2ad93597-7b54-43dd-9eb1-631dd337e5a7'
-                                          }
-                                      }
-              )
-              expect(Vcloud::Core::EdgeGateway).to receive(:get_by_name).with('1111111-7b54-43dd-9eb1-631dd337e5a7')
-                                                   .and_return(edge_gateway)
-              generated_config = NatService.new('1111111-7b54-43dd-9eb1-631dd337e5a7', test_case[:input]).generate_fog_config
+              generated_config = NatService.new(@edge_id, test_case[:input]).generate_fog_config
               expect(generated_config).to eq(test_case[:output])
             end
-
           end
 
         end
