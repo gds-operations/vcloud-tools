b5a036a75009250efbc556fcc5818af3f2f76553
Mike Pountney <Mike.Pountney@gmail.com>
2014-02-13 11:34:39 +0000

    Split long lines - no functional change

--- lib/vcloud/edge_gateway/configuration_generator/load_balancer_service.rb
+++ lib/vcloud/edge_gateway/configuration_generator/load_balancer_service.rb
@@ -12,7 +12,8 @@ module Vcloud
         def generate_fog_config(load_balancer_input_config)
           return nil if load_balancer_input_config.nil?
           vcloud_load_balancer_section = {}
-          vcloud_load_balancer_section[:IsEnabled] = load_balancer_input_config.key?(:enabled) ?
+          vcloud_load_balancer_section[:IsEnabled] =
+            load_balancer_input_config.key?(:enabled) ?
               load_balancer_input_config[:enabled].to_s : 'true'
           vcloud_pools = []
           vcloud_virtual_servers = []
@@ -35,13 +36,18 @@ module Vcloud
 
         def generate_virtual_server_entry(input_virtual_server)
           vcloud_virtual_server = {}
-          vcloud_virtual_server[:IsEnabled]   = input_virtual_server.key(:enabled) ? input_virtual_server[:enabled] : 'true'
+          vcloud_virtual_server[:IsEnabled]   =
+            input_virtual_server.key(:enabled) ? input_virtual_server[:enabled] : 'true'
           vcloud_virtual_server[:Name]        = input_virtual_server[:name]
-          vcloud_virtual_server[:Description] = input_virtual_server[:description] || ''
-          vcloud_virtual_server[:Interface]   = generate_virtual_server_interface_section(input_virtual_server[:network])
+          vcloud_virtual_server[:Description] =
+            input_virtual_server[:description] || ''
+          vcloud_virtual_server[:Interface]   =
+            generate_virtual_server_interface_section(input_virtual_server[:network])
           vcloud_virtual_server[:IpAddress]   = input_virtual_server[:ip_address]
-          vcloud_virtual_server[:ServiceProfile] = generate_virtual_server_service_profile_section(input_virtual_server[:service_profiles])
-          vcloud_virtual_server[:Logging]     = input_virtual_server.key(:logging) ? input_virtual_server[:logging] : 'false'
+          vcloud_virtual_server[:ServiceProfile] =
+            generate_virtual_server_service_profile_section(input_virtual_server[:service_profiles])
+          vcloud_virtual_server[:Logging]     =
+            input_virtual_server.key(:logging) ? input_virtual_server[:logging] : 'false'
           vcloud_virtual_server[:Pool]        = input_virtual_server[:pool]
           vcloud_virtual_server
         end
@@ -71,7 +77,11 @@ module Vcloud
           vcloud_service_profiles = []
           protocols = [ :http, :https, :tcp ]
           protocols.each do |protocol|
-            vcloud_service_profiles << generate_virtual_server_service_profile_protocol_section(protocol, input_service_profile[protocol])
+            vcloud_service_profiles <<
+              generate_virtual_server_service_profile_protocol_section(
+                protocol,
+                input_service_profile[protocol]
+              )
           end
           vcloud_service_profiles
         end
@@ -84,9 +94,17 @@ module Vcloud
             Persistence: generate_virtual_server_persistence_section(protocol, nil)
           }
           if input_protocol_section
-            vcloud_protocol_section[:IsEnabled] = input_protocol_section.key?(:enabled) ? input_protocol_section[:enabled].to_s : 'true'
-            vcloud_protocol_section[:Port] = input_protocol_section.key?(:port) ? input_protocol_section[:port].to_s : default_port(protocol)
-            vcloud_protocol_section[:Persistence] = generate_virtual_server_persistence_section(protocol, input_protocol_section[:persistence])
+            vcloud_protocol_section[:IsEnabled] =
+              input_protocol_section.key?(:enabled) ?
+                input_protocol_section[:enabled].to_s : 'true'
+            vcloud_protocol_section[:Port] =
+              input_protocol_section.key?(:port) ?
+                input_protocol_section[:port].to_s : default_port(protocol)
+            vcloud_protocol_section[:Persistence] =
+              generate_virtual_server_persistence_section(
+                protocol,
+                input_protocol_section[:persistence]
+              )
           end
           vcloud_protocol_section
         end
@@ -100,7 +118,9 @@ module Vcloud
           input_persistence_section = {} if input_persistence_section.nil?
           vcloud_persistence_section = { Method: '' }
           if input_persistence_section.key?(:method)
-            vcloud_persistence_section[:Method] = input_persistence_section[:method] if input_persistence_section.key?(:method)
+            if input_persistence_section.key?(:method)
+              vcloud_persistence_section[:Method] = input_persistence_section[:method]
+            end
             if input_persistence_section[:method] == 'COOKIE'
               vcloud_persistence_section[:CookieName] = input_persistence_section[:cookie_name]
               vcloud_persistence_section[:CookieMode] = input_persistence_section[:cookie_mode]
@@ -113,7 +133,9 @@ module Vcloud
           sp_modes = [ :http, :https, :tcp ]
           vcloud_pool_entry = {}
           vcloud_pool_entry[:Name] = input_pool_entry[:name]
-          vcloud_pool_entry[:Description] = input_pool_entry[:description] if input_pool_entry.key?(:description)
+          if input_pool_entry.key?(:description)
+            vcloud_pool_entry[:Description] = input_pool_entry[:description]
+          end
           vcloud_pool_entry[:ServicePort] = sp_modes.map do |mode|
             generate_pool_service_port(mode,
               input_pool_entry.key?(:service) ? input_pool_entry[:service][mode] : nil)
@@ -151,12 +173,20 @@ module Vcloud
           }
 
           if input_pool_service_port
-            vcloud_pool_service_port[:IsEnabled] = input_pool_service_port.key?(:enabled) ? input_pool_service_port[:enabled].to_s : 'true'
-            vcloud_pool_service_port[:Algorithm] = input_pool_service_port[:algorithm] if input_pool_service_port.key?(:algorithm)
-            vcloud_pool_service_port[:Port]      = input_pool_service_port.key?(:port) ? input_pool_service_port[:port].to_s : default_port(mode)
+            vcloud_pool_service_port[:IsEnabled] =
+              input_pool_service_port.key?(:enabled) ?
+                input_pool_service_port[:enabled].to_s : 'true'
+            if input_pool_service_port.key?(:algorithm)
+              vcloud_pool_service_port[:Algorithm] = input_pool_service_port[:algorithm]
+            end
+            vcloud_pool_service_port[:Port]      =
+              input_pool_service_port.key?(:port) ?
+                input_pool_service_port[:port].to_s : default_port(mode)
             if health_check = input_pool_service_port[:health_check]
-              vcloud_pool_service_port[:HealthCheckPort] = health_check.key?(:port) ? health_check[:port].to_s : ''
-              vcloud_pool_service_port[:HealthCheck] = generate_pool_healthcheck(mode, input_pool_service_port[:health_check])
+              vcloud_pool_service_port[:HealthCheckPort] =
+                health_check.key?(:port) ? health_check[:port].to_s : ''
+              vcloud_pool_service_port[:HealthCheck] =
+                generate_pool_healthcheck(mode, input_pool_service_port[:health_check])
             end
           end
           vcloud_pool_service_port
@@ -167,22 +197,41 @@ module Vcloud
           vcloud_pool_healthcheck_entry = {
             Mode: default_mode,
           }
-          vcloud_pool_healthcheck_entry[:Uri] = '' if protocol == :http
-          vcloud_pool_healthcheck_entry[:Uri] = '' if ( protocol == :https ) && input_pool_healthcheck_entry && ( input_pool_healthcheck_entry[:protocol] == 'TCP' )
+          if protocol == :http
+            vcloud_pool_healthcheck_entry[:Uri] = ''
+          elsif ( protocol == :https ) &&
+                  input_pool_healthcheck_entry &&
+                  ( input_pool_healthcheck_entry[:protocol] == 'TCP' )
+            vcloud_pool_healthcheck_entry[:Uri] = ''
+          end
           vcloud_pool_healthcheck_entry[:HealthThreshold] = '2'
           vcloud_pool_healthcheck_entry[:UnhealthThreshold] = '3'
           vcloud_pool_healthcheck_entry[:Interval] = '5'
           vcloud_pool_healthcheck_entry[:Timeout] = '15'
 
           if input_pool_healthcheck_entry
-            vcloud_pool_healthcheck_entry[:Mode] = input_pool_healthcheck_entry[:protocol] if input_pool_healthcheck_entry.key?(:protocol)
-            vcloud_pool_healthcheck_entry[:Uri]  = input_pool_healthcheck_entry[:uri] if input_pool_healthcheck_entry.key?(:uri) and protocol == :http
-            vcloud_pool_healthcheck_entry[:HealthThreshold] = input_pool_healthcheck_entry[:health_threshold] if
-                input_pool_healthcheck_entry.key?(:health_threshold)
-            vcloud_pool_healthcheck_entry[:UnhealthThreshold] = input_pool_healthcheck_entry[:unhealth_threshold] if
-                input_pool_healthcheck_entry.key?(:unhealth_threshold)
-            vcloud_pool_healthcheck_entry[:Interval] = input_pool_healthcheck_entry[:interval] if input_pool_healthcheck_entry.key?(:interval)
-            vcloud_pool_healthcheck_entry[:Timeout] = input_pool_healthcheck_entry[:timeout] if input_pool_healthcheck_entry.key?(:timeout)
+            if input_pool_healthcheck_entry.key?(:protocol)
+              vcloud_pool_healthcheck_entry[:Mode] = input_pool_healthcheck_entry[:protocol]
+            end
+            if input_pool_healthcheck_entry.key?(:uri) and protocol == :http
+              vcloud_pool_healthcheck_entry[:Uri]  = input_pool_healthcheck_entry[:uri]
+            end
+            if input_pool_healthcheck_entry.key?(:health_threshold)
+              vcloud_pool_healthcheck_entry[:HealthThreshold] =
+                input_pool_healthcheck_entry[:health_threshold]
+            end
+            if input_pool_healthcheck_entry.key?(:unhealth_threshold)
+              vcloud_pool_healthcheck_entry[:UnhealthThreshold] =
+                input_pool_healthcheck_entry[:unhealth_threshold]
+            end
+            if input_pool_healthcheck_entry.key?(:interval)
+              vcloud_pool_healthcheck_entry[:Interval] =
+                input_pool_healthcheck_entry[:interval]
+            end
+            if input_pool_healthcheck_entry.key?(:timeout)
+              vcloud_pool_healthcheck_entry[:Timeout] =
+                input_pool_healthcheck_entry[:timeout]
+            end
           end
           vcloud_pool_healthcheck_entry
         end
