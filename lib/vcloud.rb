2e93167ed7801865eacae4de27d3cb44a5cc0872
Anna Shipman <anna@annashipman.co.uk>
2014-03-10 14:34:25 +0000

    Remove local config loader

--- lib/vcloud.rb
+++ lib/vcloud.rb
@@ -11,7 +11,6 @@ require 'vcloud/version'
 require 'vcloud/fog'
 require 'vcloud/core'
 
-require 'vcloud/config_loader'
 require 'vcloud/launch'
 require 'vcloud/net_launch'
 require 'vcloud/vm_orchestrator'
