142f2556a1798f0851db05d5c73c48fe17ae715a
Anna Shipman <anna@annashipman.co.uk>
2014-04-17 15:34:46 +0100

    We only require Core, Fog and Version
    The gemspec will take care of installing all the gems if a user does `gem install vcloud-tools`.  However, if they want to build a script then it is better for them to explicitly require the particular tools they want to use as and when they want to use them as this will make it clearer what is going on, and will allow them to build something that only uses the tools they require.
    
    My thoughts on this inspired by what rspec does, and this specific discussion: https://github.com/rspec/rspec/commit/f10bedd498dff2961d4be9cce793fa152c4a0bbe

--- lib/vcloud.rb
+++ lib/vcloud.rb
@@ -1,26 +1,3 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'json'
-require 'yaml'
-require 'csv'
-require 'open3'
-require 'pp'
-
-require 'vcloud/version'
-
-require 'vcloud/fog'
 require 'vcloud/core'
-
-require 'vcloud/launcher'
-
-module Vcloud
-
-  def self.logger
-    @logger ||= Logger.new(STDOUT)
-  end
-
-  def self.clone_object object
-    Marshal.load(Marshal.dump(object))
-  end
-
-end
+require 'vcloud/fog'
+require 'vcloud/version'
